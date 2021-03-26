fc92b6637652743b8f48e254bc9bcba2ec0e778f
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-03-26 13:50:12 -0600

    initial app setup

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, View, StyleSheet, Text } from "react-native";
+import ToggleLoginSignUp from './containers/ToggleLoginSignUp'
+import HomePage from './containers/HomePage'
 
 export default function App() {
+  const [user, setUser] = useState({});
+  const [isOwner, setIsOwner] = useState(false)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {user.username
+        ? <HomePage user={user} setUser={setUser} isOwner={isOwner} />
+        : <ToggleLoginSignUp setUser={setUser} user={user} isOwner={isOwner} setIsOwner={setIsOwner}/>
+      }
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
-});
+})
