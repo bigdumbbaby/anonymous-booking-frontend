773d45977445c59e4947b27c59b5952186057f23
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-03-26 13:57:04 -0600

    added toggle switch

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,10 +1,121 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import ToggleSwitch from 'toggle-switch-react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  SafeAreaView
+} from "react-native";
 
-export default function LoginForm() {
+export default function LoginForm({error, setError, toggleSignUp, login, isOwner, handleToggle}) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    login(username, password)
+    console.log(username, password)
+  }
+
+  const handleLink = () => {
+    setError("")
+    toggleSignUp()
+  }
+
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>LOGIN</Text>
+      <ToggleSwitch
+        isOn={isOwner}
+        onColor="#add8e6"
+        offColor="#F5F5F5"
+        label="Venu Owner"
+        labelStyle={{ color: "black"}}
+        size="large"
+        style={styles.toggle}
+        onToggle={handleToggle}
+      />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Username."
+          placeholderTextColor="#003f5c"
+          onChangeText={(username) => setUsername(username)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password."
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+      {error !== ""
+      ? <Text style={{color: 'red'}}>{error}</Text> 
+      : null
+      }
+
+      <TouchableOpacity style={styles.loginBtn} onPress={handleSubmit}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <Text style={styles.signUpLink} onPress={handleLink}>Need to sign up?</Text>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  toggle: {
+    padding: 10,
+  },
+  signUpLink : {
+    paddingTop: 10,
+  },
+  title: {
+    paddingBottom: 30,
+  },
+  inputView: {
+    backgroundColor: "#add8e6",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+
+    alignItems: "center",
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+    width: 150
+  },
+
+  signUpButton: {
+    height: 30,
+    marginBottom: 30,
+  },
+
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#808080",
+  },
+});
