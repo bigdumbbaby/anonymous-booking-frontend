d641e2715679de3bc28ea2034818723fa2eda00e
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-04-01 11:46:16 -0600

    styling ready to present

--- components/VenueForm.js
+++ components/VenueForm.js
@@ -69,135 +69,130 @@ export default function AddVenueForm({createVenue, user}) {
   };
 
   return (
-    <ScrollView style={styles.formScroll} contentContainerStyle={{ flexGrow: 1 }}>
-      <View>
-        {image === ""
-          ? null
-          : <Image 
-          source={{uri: image}}
-          style={{width: 162, height: 200, flex: 1}}
+    <View style={styles.container}>
+
+      <ScrollView style={styles.formScroll} contentContainerStyle={{justifyContent: 'center'}}>
+          {image === ""
+            ? null
+            : <Image 
+            source={{uri: image}}
+            style={{width: 162, height: 200, flex: 1}}
+            />
+          }
+        
+        <Button title="Choose Photo" onPress={pickImage}/>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Name."
+            placeholderTextColor="#003f5c"
+            onChangeText={(name) => setName(name)}
           />
-        }
-      </View>
-      
-      <Button title="Choose Photo" onPress={pickImage}/>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Name."
-          placeholderTextColor="#003f5c"
-          onChangeText={(name) => setName(name)}
-        />
-      </View>
-      <View>
-      <Text>Venue Type:</Text>
-      <DropDownPicker 
-      style={styles.dropDown}
-      containerStyle={{height: 60, width: "70%"}}
-      items={[
-        { label: 'Show Space', value: 'Show Space' },
-        { label: 'Art Gallery', value: 'Art Gallery' },
-        { label: 'Film Screening', value: 'Film Screening' },
-        { label: 'Misc.', value: 'Misc.' },
-      ]}
-      onChangeItem={item => setType(item.value)}
-      />
+        </View>
 
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Address"
-          placeholderTextColor="#003f5c"
-          onChangeText={(address) => setAddress(address)}
+        <Text>Venue Type:</Text>
+        <DropDownPicker 
+        style={styles.dropDown}
+        containerStyle={{height: 60, width: "70%"}}
+        items={[
+          { label: 'Show Space', value: 'Show Space' },
+          { label: 'Art Gallery', value: 'Art Gallery' },
+          { label: 'Film Screening', value: 'Film Screening' },
+          { label: 'Misc.', value: 'Misc.' },
+        ]}
+        onChangeItem={item => setType(item.value)}
         />
-      </View>
-
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="City"
-          placeholderTextColor="#003f5c"
-          onChangeText={(city) => setCity(city)}
+  
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Address"
+            placeholderTextColor="#003f5c"
+            onChangeText={(address) => setAddress(address)}
+          />
+        </View>
+  
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="City"
+            placeholderTextColor="#003f5c"
+            onChangeText={(city) => setCity(city)}
+          />
+        </View>
+        <Text>State:</Text>
+        <DropDownPicker 
+        containerStyle={{height: 60, width: "70%"}}
+        style={styles.dropDown}
+        items={[
+          { label: 'AL', value: 'AL' },
+          { label: 'AK', value: 'AK' },
+          { label: 'AZ', value: 'AZ' },
+          { label: 'AR', value: 'AR' },
+          { label: 'CA', value: 'CA' },
+          { label: 'CO', value: 'CO' },
+          { label: 'CT', value: 'CT' },
+          { label: 'DE', value: 'DE' },
+          { label: 'FL', value: 'FL' },
+          { label: 'GA', value: 'GA' },
+          { label: 'HI', value: 'HI' },
+          { label: 'ID', value: 'ID' },
+          { label: 'IL', value: 'IL' },
+          { label: 'IN', value: 'IN' },
+          { label: 'IA', value: 'IA' },
+          { label: 'KS', value: 'KS' },
+          { label: 'KY', value: 'KY' },
+          { label: 'LA', value: 'LA' },
+          { label: 'ME', value: 'ME' },
+          { label: 'MD', value: 'MD' },
+          { label: 'MA', value: 'MA' },
+          { label: 'MI', value: 'MI' },
+          { label: 'MN', value: 'MN' },
+          { label: 'MS', value: 'MS' },
+          { label: 'MO', value: 'MO' },
+          { label: 'MT', value: 'MT' },
+          { label: 'NE', value: 'NE' },
+          { label: 'NV', value: 'NV' },
+          { label: 'NH', value: 'NH' },
+          { label: 'NJ', value: 'NJ' },
+          { label: 'NM', value: 'NM' },
+          { label: 'NY', value: 'NY' },
+          { label: 'NC', value: 'NC' },
+          { label: 'ND', value: 'ND' },
+          { label: 'OH', value: 'OH' },
+          { label: 'OK', value: 'OK' },
+          { label: 'OR', value: 'OR' },
+          { label: 'PA', value: 'PA' },
+          { label: 'RI', value: 'RI' },
+          { label: 'SC', value: 'SC' },
+          { label: 'SD', value: 'SD' },
+          { label: 'TN', value: 'TN' },
+          { label: 'TX', value: 'TX' },
+          { label: 'UT', value: 'UT' },
+          { label: 'VT', value: 'VT' },
+          { label: 'VA', value: 'VA' },
+          { label: 'WA', value: 'WA' },
+          { label: 'WV', value: 'WV' },
+          { label: 'WI', value: 'WI' },
+          { label: 'WY', value: 'WY' },
+          { label: 'DC', value: 'DC' },
+        ]}
+        onChangeItem={item => setState(item.value)}
         />
-      </View>
-      <View>
-      <Text>State:</Text>
-      <DropDownPicker 
-      containerStyle={{height: 60, width: "70%"}}
-      style={styles.dropDown}
-      items={[
-        { label: 'AL', value: 'AL' },
-        { label: 'AK', value: 'AK' },
-        { label: 'AZ', value: 'AZ' },
-        { label: 'AR', value: 'AR' },
-        { label: 'CA', value: 'CA' },
-        { label: 'CO', value: 'CO' },
-        { label: 'CT', value: 'CT' },
-        { label: 'DE', value: 'DE' },
-        { label: 'FL', value: 'FL' },
-        { label: 'GA', value: 'GA' },
-        { label: 'HI', value: 'HI' },
-        { label: 'ID', value: 'ID' },
-        { label: 'IL', value: 'IL' },
-        { label: 'IN', value: 'IN' },
-        { label: 'IA', value: 'IA' },
-        { label: 'KS', value: 'KS' },
-        { label: 'KY', value: 'KY' },
-        { label: 'LA', value: 'LA' },
-        { label: 'ME', value: 'ME' },
-        { label: 'MD', value: 'MD' },
-        { label: 'MA', value: 'MA' },
-        { label: 'MI', value: 'MI' },
-        { label: 'MN', value: 'MN' },
-        { label: 'MS', value: 'MS' },
-        { label: 'MO', value: 'MO' },
-        { label: 'MT', value: 'MT' },
-        { label: 'NE', value: 'NE' },
-        { label: 'NV', value: 'NV' },
-        { label: 'NH', value: 'NH' },
-        { label: 'NJ', value: 'NJ' },
-        { label: 'NM', value: 'NM' },
-        { label: 'NY', value: 'NY' },
-        { label: 'NC', value: 'NC' },
-        { label: 'ND', value: 'ND' },
-        { label: 'OH', value: 'OH' },
-        { label: 'OK', value: 'OK' },
-        { label: 'OR', value: 'OR' },
-        { label: 'PA', value: 'PA' },
-        { label: 'RI', value: 'RI' },
-        { label: 'SC', value: 'SC' },
-        { label: 'SD', value: 'SD' },
-        { label: 'TN', value: 'TN' },
-        { label: 'TX', value: 'TX' },
-        { label: 'UT', value: 'UT' },
-        { label: 'VT', value: 'VT' },
-        { label: 'VA', value: 'VA' },
-        { label: 'WA', value: 'WA' },
-        { label: 'WV', value: 'WV' },
-        { label: 'WI', value: 'WI' },
-        { label: 'WY', value: 'WY' },
-        { label: 'DC', value: 'DC' },
-      ]}
-      onChangeItem={item => setState(item.value)}
-      />
-
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Zip Code"
-          placeholderTextColor="#003f5c"
-          onChangeText={(zip) => setZip(zip)}
-        />
-      </View>
-      <View>
-      <TouchableOpacity style={styles.loginBtn} onPress={handleSubmit}>
-        <Text style={styles.loginText}>SUBMIT</Text>
-      </TouchableOpacity>
-
-      </View>
-    </ScrollView>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Zip Code"
+            placeholderTextColor="#003f5c"
+            onChangeText={(zip) => setZip(zip)}
+          />
+        </View>
+        <TouchableOpacity style={styles.loginBtn} onPress={handleSubmit}>
+          <Text style={styles.loginText}>SUBMIT</Text>
+        </TouchableOpacity>
+  
+      </ScrollView>
+    </View>
   )
 }
 
@@ -206,7 +201,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: 'column'
+    width: '100%',
+    height: 620,
   },
   formScroll: {
     backgroundColor: "#fff",
